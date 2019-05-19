47542cf43932767cb1e18759ecf57f164608e026
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-05-18 23:36:04 -0700

    Rework the new, open, and child handling.

--- file/file.go
+++ file/file.go
@@ -1,4 +1,7 @@
 // Package file implements a File API over a content-addressable blob.Store.
+//
+// Although a *File keeps track of basic stat information, this abstraction
+// differs from POSIX files in some important ways.
 package file
 
 import (
@@ -10,22 +13,143 @@ import (
 
 	"bitbucket.org/creachadair/ffs/blob"
 	"bitbucket.org/creachadair/ffs/file/wirepb"
+	"bitbucket.org/creachadair/ffs/splitter"
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"golang.org/x/xerrors"
 )
 
-type File struct {
-	name string // base name, not including directory
-	key  string // storage key
+// New constructs a new, empty File with the given options and backed by s. The
+// caller must call the new file's Flush method to ensure it is written to
+// storage. If opts == nil, defaults are chosen.
+func New(s blob.CAS, opts *NewOptions) *File {
+	if opts == nil {
+		opts = new(NewOptions)
+	}
+	return &File{
+		s:     s,
+		name:  opts.Name,
+		mode:  opts.Mode,
+		mtime: opts.ModTime,
+		data:  &fileData{sc: opts.Split},
+		xattr: make(map[string]string),
+	}
+}
 
-	s      blob.CAS
-	offset int64
-	mode   os.FileMode
-	mtime  time.Time
-	data   *fileData
-	kids   []*File
-	xattr  map[string]string
+// NewOptions control the creation of new files.
+type NewOptions struct {
+	Name    string
+	Mode    os.FileMode
+	ModTime time.Time
+	Split   splitter.Config
+}
+
+// Open opens an existing file given its storage key in s.
+func Open(ctx context.Context, s blob.CAS, key string) (*File, error) {
+	var node wirepb.Node
+	if err := loadProto(ctx, s, key, &node); err != nil {
+		return nil, xerrors.Errorf("loading file %q: %w", key, err)
+	}
+	f := &File{s: s, key: key}
+	f.fromProto(&node)
+	return f, nil
+}
+
+// A File represents a writable file stored in a content-addressable blobstore.
+type File struct {
+	s    blob.CAS
+	name string // if this file is a child, its attributed name
+	key  string // the storage key for the file record (wirepb.Node)
+
+	offset int64       // current seek position (≥ 0)
+	mode   os.FileMode // file mode; not used, but persisted
+	mtime  time.Time   // timestamp of last content modification
+
+	data  *fileData         // binary file data
+	kids  []child           // ordered lexicographically by name
+	xattr map[string]string // extended attributes
+}
+
+// A child records the name and storage key of a child file.
+type child struct {
+	name string
+	key  string
+}
+
+type byName []child
+
+func (b byName) Len() int           { return len(b) }
+func (b byName) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b byName) Less(i, j int) bool { return b[i].name < b[j].name }
+
+// findChild reports whether f has a child with the specified name and its
+// index in the slice if so, or otherwise -1.
+func (f *File) findChild(name string) (int, bool) {
+	if n := sort.Search(len(f.kids), func(i int) bool {
+		return f.kids[i].name == name
+	}); n < len(f.kids) {
+		return n, true
+	}
+	return -1, false
+}
+
+func (f *File) modify() { f.key = ""; f.mtime = time.Now() }
+
+// HasChild reports whether f has a child with the given name.
+func (f *File) HasChild(name string) bool { _, ok := f.findChild(name); return ok }
+
+// SetChild makes c a child of f under the given name. This operation flushes c
+// if necessary, and reports an error if that fails.
+func (f *File) SetChild(ctx context.Context, name string, c *File) error {
+	ckey, err := c.Flush(ctx)
+	if err != nil {
+		return err
+	}
+	if i, ok := f.findChild(name); ok {
+		if f.kids[i].key != ckey {
+			f.kids[i].key = ckey
+			f.modify()
+		}
+		return nil
+	}
+	f.kids = append(f.kids, child{name: name, key: ckey})
+	sort.Sort(byName(f.kids))
+	f.modify()
+	return nil
+}
+
+// RemoveChild removes name as a child of f if present, and reports whether any
+// change was made.
+func (f *File) RemoveChild(name string) bool {
+	if i, ok := f.findChild(name); ok {
+		defer f.modify()
+		f.kids = append(f.kids[:i], f.kids[i+1:]...)
+		return true
+	}
+	return false
+}
+
+// OpenChild opens the specified child file of f.
+func (f *File) OpenChild(ctx context.Context, name string) (*File, error) {
+	i, ok := f.findChild(name)
+	if !ok {
+		return nil, xerrors.Errorf("child file %q not found", name)
+	}
+	f, err := Open(ctx, f.s, f.kids[i].key)
+	if err == nil {
+		f.name = name // remember the name the file was opened with
+	}
+	return f, err
+}
+
+// Stat returns an os.FileInfo describing f.
+func (f *File) Stat() Stat {
+	return Stat{
+		name:  f.name,
+		size:  f.data.size(),
+		mode:  f.mode,
+		mtime: f.mtime,
+	}
 }
 
 // Seek sets the starting offset for the next Read or Write, as io.Seeker.
@@ -59,9 +183,9 @@ func (f *File) Read(ctx context.Context, data []byte) (int, error) {
 // Write writes len(data) bytes from data to the f at its current offset,
 // and reports the number of bytes successfully written, as io.Writer.
 func (f *File) Write(ctx context.Context, data []byte) (int, error) {
+	defer f.modify()
 	nw, err := f.data.writeAt(ctx, f.s, data, f.offset)
 	f.offset += int64(nw)
-	f.key = ""
 	return nw, err
 }
 
@@ -74,92 +198,84 @@ func (f *File) ReadAt(ctx context.Context, data []byte, offset int64) (int, erro
 // WriteAt writes len(data) bytes from data at the given offset, and reports
 // the number of bytes successfully written, as io.WriterAt.
 func (f *File) WriteAt(ctx context.Context, data []byte, offset int64) (int, error) {
-	f.key = ""
+	defer f.modify()
 	return f.data.writeAt(ctx, f.s, data, offset)
 }
 
 // Flush flushes the current state of the file to storage if necessary, and
 // returns the resulting storage key.
 func (f *File) Flush(ctx context.Context) (string, error) {
-	if f.key != "" {
-		return f.key, nil
+	if f.key == "" {
+		key, err := saveProto(ctx, f.s, f.toProto())
+		if err != nil {
+			return "", xerrors.Errorf("flushing file %q: %w", key, err)
+		}
+		f.key = key
 	}
-	key, err := saveNode(ctx, f.s, f.toProto())
-	if err != nil {
-		return "", err
-	}
-	f.key = key
-	return key, nil
+	return f.key, nil
 }
 
 func (f *File) fromProto(pb *wirepb.Node) {
-	f.name = pb.GetName()
+	f.data.fromProto(pb.Index)
 	f.mode = os.FileMode(pb.GetMode())
-	f.kids = nil
-	f.xattr = make(map[string]string)
 
 	if ts, err := ptypes.Timestamp(pb.ModTime); err == nil {
 		f.mtime = ts
 	}
-	for _, xa := range pb.Xattrs {
+
+	f.xattr = make(map[string]string)
+	for _, xa := range pb.XAttrs {
 		f.xattr[xa.GetName()] = string(xa.GetValue())
 	}
+
+	f.kids = nil
 	for _, kid := range pb.Children {
-		f.kids = append(f.kids, &File{
+		f.kids = append(f.kids, child{
 			name: kid.GetName(),
 			key:  string(kid.GetKey()),
 		})
 	}
+	sort.Sort(byName(f.kids))
 }
 
 func (f *File) toProto() *wirepb.Node {
 	n := &wirepb.Node{
-		Name: f.name,
-		Mode: uint32(f.mode),
-		Data: f.data.toProto(),
+		Mode:  uint32(f.mode),
+		Index: f.data.toProto(),
 	}
 	if ts, err := ptypes.TimestampProto(f.mtime); err == nil && !f.mtime.IsZero() {
 		n.ModTime = ts
 	}
 	for name, value := range f.xattr {
-		n.Xattrs = append(n.Xattrs, &wirepb.XAttr{
+		n.XAttrs = append(n.XAttrs, &wirepb.XAttr{
 			Name:  name,
 			Value: []byte(value),
 		})
 	}
+	sort.Slice(n.XAttrs, func(i, j int) bool {
+		return n.XAttrs[i].Name < n.XAttrs[j].Name
+	})
 	for _, kid := range f.kids {
 		n.Children = append(n.Children, &wirepb.Child{
 			Name: kid.name,
 			Key:  []byte(kid.key),
 		})
 	}
-	sort.Slice(n.Xattrs, func(i, j int) bool {
-		return n.Xattrs[i].Name < n.Xattrs[j].Name
-	})
-	sort.Slice(n.Children, func(i, j int) bool {
-		return n.Children[i].Name < n.Children[j].Name
-	})
 	return n
 }
 
-/*
-func loadNode(ctx context.Context, s blob.CAS, key string) (*wirepb.Node, error) {
-	bits, err := s.Get(ctx, key)
+func saveProto(ctx context.Context, s blob.CAS, msg proto.Message) (string, error) {
+	bits, err := proto.Marshal(msg)
 	if err != nil {
-		return nil, xerrors.Errorf("loading node: %w", err)
-	}
-	pb := new(wirepb.Node)
-	if err := proto.Unmarshal(bits, pb); err != nil {
-		return nil, xerrors.Errorf("decoding node: %w", err)
-	}
-	return pb, nil
-}
-*/
-
-func saveNode(ctx context.Context, s blob.CAS, pb *wirepb.Node) (string, error) {
-	bits, err := proto.Marshal(pb)
-	if err != nil {
-		return "", xerrors.Errorf("encoding node: %w", err)
+		return "", xerrors.Errorf("encoding message: %w", err)
 	}
 	return s.PutCAS(ctx, bits)
+}
+
+func loadProto(ctx context.Context, s blob.CAS, key string, msg proto.Message) error {
+	bits, err := s.Get(ctx, key)
+	if err != nil {
+		return xerrors.Errorf("loading message: %w", err)
+	}
+	return proto.Unmarshal(bits, msg)
 }
