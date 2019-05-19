d976c99cf16f2dd99b868280f258fa3fbdb8e2ae
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-05-19 01:17:14 -0700

    Add a basic children structure test.

--- file/file_test.go
+++ file/file_test.go
@@ -15,12 +15,11 @@ import (
 	"bitbucket.org/creachadair/ffs/file/wirepb"
 	"bitbucket.org/creachadair/ffs/splitter"
 	"github.com/golang/protobuf/proto"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestNewStat(t *testing.T) {
-	mem := memstore.New()
-	cas := blob.NewCAS(mem, sha1.New)
-
+	cas := newCAS()
 	f := file.New(cas, &file.NewOptions{
 		Name: "testfile",
 		Mode: 0644,
@@ -47,8 +46,7 @@ func TestNewStat(t *testing.T) {
 }
 
 func TestRoundTrip(t *testing.T) {
-	mem := memstore.New()
-	cas := blob.NewCAS(mem, sha1.New)
+	cas := newCAS()
 	f := file.New(cas, &file.NewOptions{
 		Mode:  0640,
 		Split: splitter.Config{Min: 17, Size: 84, Max: 500},
@@ -56,12 +54,7 @@ func TestRoundTrip(t *testing.T) {
 	ctx := context.Background()
 
 	const testMessage = "Four fat fennel farmers fell feverishly for Felicia Frances"
-	io.WriteString(f.IO(ctx), testMessage)
-	fkey, err := f.Flush(ctx)
-	if err != nil {
-		t.Fatalf("Flush failed: %v", err)
-	}
-	t.Logf("Flushed output file to %s", fmtKey(fkey))
+	fkey := mustWrite(t, f, testMessage)
 
 	g, err := file.Open(ctx, cas, fkey)
 	if err != nil {
@@ -75,7 +68,58 @@ func TestRoundTrip(t *testing.T) {
 		t.Errorf("Reading %s: got %q, want %q", fmtKey(fkey), got, testMessage)
 	}
 
-	bits, err = mem.Get(ctx, fkey)
+	logIndex(t, cas, fkey)
+}
+
+func TestChildren(t *testing.T) {
+	cas := newCAS()
+	ctx := context.Background()
+	root := file.New(cas, nil)
+
+	f := file.New(cas, nil)
+	fkey := mustWrite(t, f, "higgledy piggledy")
+	if err := root.SetChild(ctx, "foo", f); err != nil {
+		t.Fatalf("SetChild failed: %v", err)
+	}
+
+	rkey, err := root.Flush(ctx)
+	if err != nil {
+		t.Fatalf("Flush root failed: %v", err)
+	}
+
+	t.Logf("Root key %s, child key %s", fmtKey(rkey), fmtKey(fkey))
+	got := root.Children()
+	if diff := cmp.Diff([]file.Child{{
+		Name: "foo",
+		Key:  fkey,
+	}}, got); diff != "" {
+		t.Errorf("Children of root (-want, +got):\n%s", diff)
+	}
+
+	logIndex(t, cas, rkey)
+}
+
+func fmtKey(s string) string { return base64.RawURLEncoding.EncodeToString([]byte(s)) }
+
+func newCAS() blob.CAS { return blob.NewCAS(memstore.New(), sha1.New) }
+
+func mustWrite(t *testing.T, f *file.File, s string) string {
+	t.Helper()
+	ctx := context.Background()
+	if _, err := io.WriteString(f.IO(ctx), s); err != nil {
+		t.Fatalf("Writing %q failed: %v", s, err)
+	}
+	key, err := f.Flush(ctx)
+	if err != nil {
+		t.Fatalf("Flushing %q failed: %v", fmtKey(key), err)
+	}
+	return key
+}
+
+func logIndex(t *testing.T, cas blob.CAS, fkey string) {
+	t.Helper()
+	ctx := context.Background()
+	bits, err := cas.Get(ctx, fkey)
 	if err != nil {
 		t.Fatalf("Reading %s from storage: %v", fmtKey(fkey), err)
 	}
@@ -84,8 +128,4 @@ func TestRoundTrip(t *testing.T) {
 		t.Errorf("Decoding wire node: %v", err)
 	}
 	t.Log("Index:\n", proto.MarshalTextString(node))
-}
-
-func fmtKey(s string) string {
-	return base64.RawURLEncoding.EncodeToString([]byte(s))
 }
