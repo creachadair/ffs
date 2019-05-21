53f7b9135ebcdb5e94226ee75f3615715fc9b2eb
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-05-20 23:40:04 -0700

    Add package fpath, implementing path traversal.

---
+++ file/fpath/fpath.go
@@ -0,0 +1,123 @@
+// Copyright 2019 Michael J. Fromberger. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package fpath implements path traversal relative to a *file.File.  A path is
+// a slash-separated string,
+package fpath
+
+import (
+	"context"
+	"strings"
+
+	"bitbucket.org/creachadair/ffs/file"
+	"golang.org/x/xerrors"
+)
+
+// Open traverses the given slash-separated path sequentially from root, and
+// returns the resulting file or ErrChildNotFound. An empty path yields root
+// without error.
+func Open(ctx context.Context, root *file.File, path string) (*file.File, error) {
+	fp, err := findPath(ctx, query{root: root, path: path})
+	return fp.target, err
+}
+
+// Set traverses the given slash-separated path sequentially from root and
+// inserts f at the end of it. An empty path is an error, and if any element of
+// the path except the last does not exist it reports ErrChildNotFound.
+func Set(ctx context.Context, root *file.File, path string, f *file.File) error {
+	dir, base := "", path
+	if i := strings.LastIndex(path, "/"); i > 0 {
+		dir, base = path[:i], path[i+1:]
+	}
+	fp, err := findPath(ctx, query{root: root, path: dir})
+	if err != nil {
+		return err
+	}
+	return fp.target.Set(ctx, base, f)
+}
+
+// Create creates a file at the given slash-separated path beneath root,
+// creating any necessary parents, and returns the final element named. An
+// empty path yields root without error.
+func Create(ctx context.Context, root *file.File, path string) (*file.File, error) {
+	fp, err := findPath(ctx, query{
+		root: root,
+		path: path,
+		ef: func(fp *foundPath, err error) error {
+			if xerrors.Is(err, file.ErrChildNotFound) {
+				c := fp.target.New(&file.NewOptions{Name: fp.targetName})
+				err = fp.target.Set(ctx, fp.targetName, c)
+				fp.parent, fp.target = fp.target, c
+			}
+			return err
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return fp.target, nil
+}
+
+// Remove removes the file at the given slash-separated path beneath root.
+// If any component of the path does not exist, it returns ErrChildNotFound.
+func Remove(ctx context.Context, root *file.File, path string) error {
+	fp, err := findPath(ctx, query{root: root, path: path})
+	if err != nil {
+		return err
+	} else if fp.parent != nil {
+		fp.parent.Remove(fp.targetName)
+	}
+	return nil
+}
+
+type errFilter = func(*foundPath, error) error
+
+func findPath(ctx context.Context, q query) (foundPath, error) {
+	fp := foundPath{
+		parent: nil,
+		target: q.root,
+	}
+	for _, name := range parsePath(q.path) {
+		fp.targetName = name
+		c, err := fp.target.Open(ctx, name)
+		if err == nil {
+			fp.parent, fp.target = fp.target, c
+		} else if q.ef == nil {
+			return fp, err
+		} else if ferr := q.ef(&fp, err); ferr != nil {
+			return fp, ferr
+		}
+	}
+	return fp, nil
+}
+
+type query struct {
+	root *file.File
+	path string
+	ef   errFilter
+}
+
+type foundPath struct {
+	parent     *file.File
+	target     *file.File
+	targetName string
+}
+
+func parsePath(path string) []string {
+	clean := strings.TrimPrefix(path, "/")
+	if clean == "" {
+		return nil
+	}
+	return strings.Split(clean, "/")
+}
---
+++ file/fpath/fpath_test.go
@@ -0,0 +1,115 @@
+package fpath_test
+
+import (
+	"context"
+	"crypto/sha1"
+	"testing"
+
+	"bitbucket.org/creachadair/ffs/blob"
+	"bitbucket.org/creachadair/ffs/blob/memstore"
+	"bitbucket.org/creachadair/ffs/file"
+	"bitbucket.org/creachadair/ffs/file/fpath"
+	"golang.org/x/xerrors"
+)
+
+func TestPaths(t *testing.T) {
+	cas := blob.NewCAS(memstore.New(), sha1.New)
+
+	ctx := context.Background()
+	root := file.New(cas, nil)
+	createPath := func(path string, werr error) *file.File {
+		got, err := fpath.Create(ctx, root, path)
+		if !errorOK(err, werr) {
+			t.Errorf("CreatePath %q: got error %v, want %v", path, err, werr)
+		}
+		return got
+	}
+	removePath := func(path string, werr error) {
+		err := fpath.Remove(ctx, root, path)
+		if !errorOK(err, werr) {
+			t.Errorf("RemovePath %q: got error %v, want %v", path, err, werr)
+		}
+	}
+	openPath := func(path string, werr error) *file.File {
+		got, err := fpath.Open(ctx, root, path)
+		if !errorOK(err, werr) {
+			t.Errorf("OpenPath %q: got error %v, want %v", path, err, werr)
+		}
+		return got
+	}
+	setPath := func(path string, f *file.File, werr error) {
+		err := fpath.Set(ctx, root, path, f)
+		if !errorOK(err, werr) {
+			t.Errorf("SetPath %q: got error %v, want %v", path, err, werr)
+		}
+	}
+
+	// Opening the empty path should return the root.
+	if got := openPath("", nil); got != root {
+		t.Errorf("Open empty path: got %p, want %p", got, root)
+	}
+
+	// Removing or creating an empty path should quietly do nothing.
+	removePath("", nil)
+	removePath("/", nil)
+	createPath("", nil)
+	createPath("/", nil)
+
+	// Removing non-existing non-empty paths should report an error,
+	removePath("nonesuch", file.ErrChildNotFound)
+	removePath("/no/such/path", file.ErrChildNotFound)
+
+	// Opening a non-existing path should report an error.
+	openPath("/a/lasting/peace", file.ErrChildNotFound)
+
+	// After creating a path, we should be able to open it and get back the same
+	// file value we created.
+	{
+		want := createPath("/a/lasting/peace", nil)
+		got := openPath("/a/lasting/peace", nil)
+		if got != want {
+			t.Errorf("Open returned the wrong file: got %+v, want %+v", got, want)
+		}
+	}
+
+	// Prefixes of an existing path should exist.
+	openPath("/a", nil)
+	openPath("/a/lasting", nil)
+
+	// Non-existing siblings should report an error.
+	openPath("/a/lasting/war", file.ErrChildNotFound)
+
+	// Creating a sibling should work, and not disturb its neighbors.
+	createPath("/a/lasting/consequence", nil)
+	openPath("/a/lasting/peace", nil)
+	openPath("/a/lasting/consequence", nil)
+
+	// Removing a path should make it unreachable.
+	removePath("/a/lasting/peace", nil)
+	openPath("/a/lasting/peace", file.ErrChildNotFound)
+
+	createPath("/a/lasting/war/of/words", nil)
+	subtree := openPath("/a/lasting/war", nil)
+	openPath("/a/lasting/war/of", nil)
+	openPath("/a/lasting/war/of/words", nil)
+
+	// Removing an intermediate node drops the whole subtree, but not its ancestor.
+	removePath("/a/lasting/war", nil)
+	openPath("/a/lasting/war", file.ErrChildNotFound)
+	openPath("/a/lasting/war/of", file.ErrChildNotFound)
+	openPath("/a/lasting/war/of/words", file.ErrChildNotFound)
+
+	// A subtree can be spliced in, and preserve its structure.
+	createPath("/a/boring", nil)
+	setPath("/a/boring/war", subtree, nil)
+	openPath("/a/boring/war", nil)
+	openPath("/a/boring/war/of", nil)
+	openPath("/a/boring/war/of/words", nil)
+}
+
+func errorOK(err, werr error) bool {
+	if werr == nil {
+		return err == nil
+	}
+	return xerrors.Is(err, werr)
+}
