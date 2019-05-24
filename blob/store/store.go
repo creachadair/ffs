a46edf88f570d9a9744cef61d0fdd31f1a31adfc
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-05-24 13:25:38 -0700

    Add package store, a storage registry.

---
+++ blob/store/store.go
@@ -0,0 +1,77 @@
+// Package store provides an interface to open blob.Store instances named by
+// string addresses or URLs.
+package store
+
+import (
+	"context"
+	"strings"
+	"sync"
+
+	"bitbucket.org/creachadair/ffs/blob"
+	"golang.org/x/xerrors"
+)
+
+// Default is the default store registry.
+var Default = Registry{}
+
+// An Opener opens a blob.Store instance associated with the given address.  An
+// Opener must be safe for concurrent use by multiple goroutines.
+type Opener func(ctx context.Context, addr string) (blob.Store, error)
+
+// A Registry maintains a mapping from addresses to Opener values.  The methods
+// of a Registry are safe for concurrent use by multiple goroutines.
+type Registry struct {
+	μ sync.RWMutex
+	m map[string]Opener
+}
+
+// Register associates the specified address tag with the given Opener.  It is
+// an error (ErrDuplicateTag) if tag is already registered.
+// A tag may end with ":" but must not otherwise contain any ":" characters.
+func (r *Registry) Register(tag string, o Opener) error {
+	clean := strings.TrimSuffix(tag, ":")
+	if clean == "" || strings.HasSuffix(clean, ":") {
+		return xerrors.Errorf("register %q: invalid tag", tag)
+	} else if o == nil {
+		return xerrors.Errorf("register %q: opener is nil", tag)
+	}
+
+	r.μ.Lock()
+	defer r.μ.Unlock()
+	if r.m == nil {
+		r.m = make(map[string]Opener)
+	} else if _, ok := r.m[clean]; ok {
+		return xerrors.Errorf("register %q: %w", clean, ErrDuplicateTag)
+	}
+	r.m[clean] = o
+	return nil
+}
+
+// Open opens a blob.Store for the specified address of the form "tag" or
+// "tag:value".  If the address does not have this form, or if the tag does not
+// correspond to any known implementation, Open reports ErrInvalidAddress.
+func (r *Registry) Open(ctx context.Context, addr string) (blob.Store, error) {
+	tag := addr
+	if i := strings.Index(addr, ":"); i >= 0 {
+		tag = addr[:i]
+	}
+
+	r.μ.RLock()
+	open, ok := r.m[tag]
+	r.μ.RUnlock()
+
+	if !ok {
+		return nil, xerrors.Errorf("open %q: %w", addr, ErrInvalidAddress)
+	}
+	return open(ctx, addr)
+}
+
+var (
+	// ErrDuplicateTag is reported by Register when given a tag which was
+	// already previously registered with a different value.
+	ErrDuplicateTag = xerrors.New("duplicate tag")
+
+	// ErrInvalidAddress is reported by Open when given an address that is
+	// syntactically invalid or has no corresponding Opener.
+	ErrInvalidAddress = xerrors.New("invalid address")
+)
---
+++ blob/store/store_test.go
@@ -0,0 +1,62 @@
+package store_test
+
+import (
+	"context"
+	"testing"
+
+	"bitbucket.org/creachadair/ffs/blob"
+	"bitbucket.org/creachadair/ffs/blob/memstore"
+	"bitbucket.org/creachadair/ffs/blob/store"
+	"golang.org/x/xerrors"
+)
+
+var badAddress = xerrors.New("bad memstore address")
+
+func newMemStore(_ context.Context, addr string) (blob.Store, error) {
+	if addr != "mem" && addr != "mem:" {
+		return nil, badAddress
+	}
+	return memstore.New(), nil
+}
+
+func TestRegisterOpen(t *testing.T) {
+	r := new(store.Registry)
+
+	// Registering a fresh name should succeed.
+	if err := r.Register("mem", newMemStore); err != nil {
+		t.Errorf("Register(mem, ...) failed: %v", err)
+	}
+
+	// Registering a fresh name with a nil Opener should fail.
+	if err := r.Register("bogus", nil); err == nil {
+		t.Error("Register(bogus, nil) did not fail")
+	} else {
+		t.Logf("Register(bogus, nil) correctly failed: %v", err)
+	}
+
+	// Re-registering an existing name should fail.
+	if err := r.Register("mem:", newMemStore); !xerrors.Is(err, store.ErrDuplicateTag) {
+		t.Errorf("Register(mem:, ...): got %v, want %v", err, store.ErrDuplicateTag)
+	}
+
+	ctx := context.Background()
+
+	// Opening an existing tag should succeed.
+	if s, err := r.Open(ctx, "mem"); err != nil {
+		t.Errorf("Open(ctx, mem) failed: %v", err)
+	} else {
+		t.Logf("Open OK, store=%[1]T (%[1]p)", s)
+	}
+
+	// Errors reported by the opener should be propagated.
+	if s, err := r.Open(ctx, "mem:garbage"); err != badAddress {
+		t.Errorf("Open(ctx, mem:garbage): got (%[1]T (%[1]p), %v), want (nil, %v)", s, err, badAddress)
+	}
+
+	// Opening a non-existing tag should fail.
+	s, err := r.Open(ctx, "http://localhost:8080")
+	if !xerrors.Is(err, store.ErrInvalidAddress) || s != nil {
+		t.Errorf("Open(ctx, URL): got (%[1]T (%[1]p), %v), want (nil, %v)",
+			s, err, store.ErrInvalidAddress)
+	}
+}
