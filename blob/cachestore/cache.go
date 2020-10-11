5b685b7421b2724c06b89d1d464e6b216d9acfb6
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-10-10 22:00:51 -0700

    Add package cachestore.
    This is an implementation of the blob.Store interface that delegates to another
    store through an in-memory cache.

---
+++ blob/cachestore/cache.go
@@ -0,0 +1,179 @@
+// Copyright 2020 Michael J. Fromberger. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cachestore
+
+// newCache constructs a new empty cache with the given capacity in bytes.  The
+// cache is not safe for concurrent use without external synchronization.
+//
+// The cache implements a "less recently used" eviction policy; new blocks are
+// inserted at the front of the cache, and cache hits move blocks toward the
+// front. Evictions occur from the rear.
+func newCache(maxBytes int) *cache {
+	sentinel := &entry{key: "sentinel"}
+	return &cache{
+		pos:     make(map[string]*entry),
+		entries: sentinel,
+		tail:    sentinel,
+		cap:     maxBytes,
+	}
+}
+
+type cache struct {
+	pos     map[string]*entry
+	entries *entry
+	tail    *entry
+	size    int // resident size in bytes
+	cap     int // capacity in bytes
+}
+
+func (c *cache) clear() {
+	c.entries.link = nil
+	c.tail = c.entries
+	c.size = 0
+	c.pos = make(map[string]*entry)
+}
+
+// drop discards the entry for key if it exists.
+func (c *cache) drop(key string) {
+	if elt := c.pos[key]; elt != nil {
+		c.discard(elt)
+	}
+}
+
+// empty reports whether the entry list is empty.
+func (c *cache) empty() bool { return c.entries.link == nil }
+
+// get reports whether any data are cached for key, and if so returns the data.
+func (c *cache) get(key string) ([]byte, bool) {
+	elt := c.pos[key]
+	if elt == nil {
+		return nil, false
+	} else if elt.link == nil || elt.link.key != key {
+		panic("cache integrity failure: " + key)
+	}
+	defer c.update(elt)
+	return copyOf(elt.link.val), true
+}
+
+// update moves elt forward in the cache to reduce the likelihood that it will
+// be evicted. This does nothing if elt is already at the head.
+func (c *cache) update(elt *entry) {
+	cur := elt.link
+	next := elt.link.link
+	if next != nil {
+		c.pos[cur.key] = cur
+		c.pos[next.key] = elt
+		cur.key, next.key = next.key, cur.key
+		cur.val, next.val = next.val, cur.val
+	}
+}
+
+func (c *cache) discard(elt *entry) int {
+	cur := elt.link
+
+	// If we're discarding the last element, update the tail.
+	// Otherwise, update the owner of the subsequent entry.
+	if c.tail == cur {
+		c.tail = elt
+	} else {
+		c.pos[cur.link.key] = elt
+	}
+
+	// Splice out the discarded entry and update sizes.
+	elt.link = cur.link
+	cur.link = nil
+	c.size -= len(cur.val)
+	delete(c.pos, cur.key)
+	return len(cur.val)
+}
+
+// evict discards eldest entries until there is room enough for increase
+// additional bytes. Precondition: increase <= c.cap.
+func (c *cache) evict(increase int) {
+	newSize := c.size + increase
+	for newSize > c.cap && !c.empty() {
+		newSize -= c.discard(c.entries)
+	}
+}
+
+// precondition: len(val) < c.cap; key is not present.
+func (c *cache) putNew(key string, val []byte) {
+	elt := &entry{key: key, val: copyOf(val)}
+	c.tail.link = elt
+	c.pos[key] = c.tail
+	c.tail = elt
+	c.size += len(val)
+}
+
+// put adds or updates key in the cache if it fits.
+func (c *cache) put(key string, val []byte) {
+	elt := c.pos[key]
+	if elt == nil {
+		// Case 1: The key is not already cached.
+		//
+		// 1a: The value is too big to ever be cached.
+		if len(val) > c.cap {
+			return
+		}
+
+		// 1b: Evict blocks as necessary for adding the new value to fit.
+		c.evict(len(val))
+		c.putNew(key, val)
+		return
+	}
+
+	// Case 2: The key is already cached, and the new value is the same or
+	// lesser size than the existing one. No eviction is required.
+	if len(val) <= len(elt.link.val) {
+		c.size = (c.size - len(elt.link.val)) + len(val)
+		elt.link.val = append(elt.link.val[:0], val...) // re-use the existing array
+		c.update(elt)
+		return
+	}
+
+	// Case 3a: The key is already cached, but the new value is too big.
+	if len(val) > c.cap {
+		c.discard(elt)
+		return
+	}
+
+	// Case 3b: Evict blocks as necessary for the new value to fit.
+	c.evict(len(val) - len(elt.link.val))
+
+	// Eviction may have moved or discarded the original location.
+	if elt := c.pos[key]; elt == nil {
+		c.putNew(key, val)
+	} else {
+		// The old block is still around; update value and sizes.
+		c.size = (c.size - len(elt.link.val)) + len(val)
+		elt.link.val = append(elt.link.val[:0], val...) // re-use the existing array
+		c.update(elt)
+	}
+}
+
+type entry struct {
+	key  string
+	val  []byte
+	link *entry
+}
+
+func copyOf(buf []byte) []byte {
+	if len(buf) == 0 {
+		return buf
+	}
+	cp := make([]byte, len(buf))
+	copy(cp, buf)
+	return cp
+}
---
+++ blob/cachestore/cachestore.go
@@ -0,0 +1,126 @@
+// Copyright 2020 Michael J. Fromberger. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package cachestore implements the blob.Store that delegates to an underlying
+// store through an in-memory cache.
+package cachestore
+
+import (
+	"context"
+	"errors"
+	"sync"
+
+	"github.com/creachadair/ffs/blob"
+)
+
+// Store implements the blob.Store interface that delegates to an underlying
+// store through an in-memory cache.
+type Store struct {
+	base blob.Store
+
+	μ      sync.Mutex
+	nexist map[string]bool // non-existence cache
+	cache  *cache          // blob cache
+}
+
+// New constructs a new cache with the specified capacity in bytes.
+// It will panic if maxBytes < 0.
+func New(s blob.Store, maxBytes int) *Store {
+	return &Store{
+		base:   s,
+		nexist: make(map[string]bool),
+		cache:  newCache(maxBytes),
+	}
+}
+
+// Get implements a method of blob.Store.
+func (s *Store) Get(ctx context.Context, key string) ([]byte, error) {
+	s.μ.Lock()
+	defer s.μ.Unlock()
+	if s.nexist[key] {
+		return nil, blob.ErrKeyNotFound
+	} else if data, ok := s.cache.get(key); ok {
+		return data, nil
+	}
+	data, err := s.base.Get(ctx, key)
+	if err != nil {
+		if errors.Is(err, blob.ErrKeyNotFound) {
+			s.nexist[key] = true
+		}
+		return nil, err
+	}
+	s.cache.put(key, data)
+	return data, nil
+}
+
+// Put implements a method of blob.Store.
+func (s *Store) Put(ctx context.Context, opts blob.PutOptions) error {
+	s.μ.Lock()
+	defer s.μ.Unlock()
+	if err := s.base.Put(ctx, opts); err != nil {
+		return err
+	}
+	s.cache.put(opts.Key, opts.Data)
+	delete(s.nexist, opts.Key)
+	return nil
+}
+
+// Delete implements a method of blob.Store.
+func (s *Store) Delete(ctx context.Context, key string) error {
+	s.μ.Lock()
+	defer s.μ.Unlock()
+	if err := s.base.Delete(ctx, key); err != nil {
+		return err
+	}
+	// N.B. Although a successful Delete certifies the key does not exist, don't
+	// record it here. We don't want to wind up with every key in the store,
+	// just the ones that were interesting enough to fetch.
+	s.cache.drop(key)
+	return nil
+}
+
+// Size implements a method of blob.Store.
+func (s *Store) Size(ctx context.Context, key string) (int64, error) {
+	s.μ.Lock()
+	defer s.μ.Unlock()
+	if s.nexist[key] {
+		return 0, blob.ErrKeyNotFound
+	}
+	size, err := s.base.Size(ctx, key)
+	if errors.Is(err, blob.ErrKeyNotFound) {
+		s.nexist[key] = true
+	}
+	return size, err
+}
+
+// List implements a method of blob.Store.
+func (s *Store) List(ctx context.Context, start string, f func(string) error) error {
+	return s.base.List(ctx, start, f)
+}
+
+// Len implements a method of blob.Store.
+func (s *Store) Len(ctx context.Context) (int64, error) {
+	return s.base.Len(ctx)
+}
+
+// Close implements blob.Closer by closing the underlying store.
+func (s *Store) Close(ctx context.Context) error {
+	s.μ.Lock()
+	defer s.μ.Unlock()
+
+	// Release the memory held by the caches.
+	s.cache.clear()
+	s.nexist = nil
+	return blob.CloseStore(ctx, s.base)
+}
