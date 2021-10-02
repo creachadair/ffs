6aa0cb478ce241ef4b08d729045ac0cc8a31ef9e
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-10-02 14:17:14 -0700

    Separate some support code into a new file.

---
+++ file/blobs.go
@@ -0,0 +1,116 @@
+package file
+
+import "io"
+
+// splitExtent splits ext into possibly-multiple extents by removing
+// zero-valued data blocks. If there are no zero blocks, the return slice
+// contains just the original extent.
+func splitExtent(ext *extent) []*extent {
+	var chunks [][]cblock
+	var bases []int64
+	var sizes []int64
+
+	// Do a two-finger walk of the blocks. The left finger (lo) scans for the
+	// next non-zero block, and the right finger (hi) scans forward from there
+	// to find the end of the non-zero range. Along the way, we keep track of
+	// the base and size of each non-zero range, to pack into extents.
+
+	base := ext.base
+	lo := 0
+
+nextChunk:
+	for lo < len(ext.blocks) {
+		// Scan for a nonzero block.
+		if ext.blocks[lo].key == "" {
+			base += ext.blocks[lo].bytes
+			lo++
+			continue
+		}
+
+		// Scan forward for a zero block.
+		nextBase := base + ext.blocks[lo].bytes
+		for hi := lo + 1; hi < len(ext.blocks); hi++ {
+			blk := ext.blocks[hi]
+
+			// If we found a zero-value block, the non-zero blocks since the last
+			// marker are an extent.
+			if blk.key == "" {
+				chunks = append(chunks, ext.blocks[lo:hi])
+				bases = append(bases, base)
+				sizes = append(sizes, nextBase-base)
+				base = nextBase
+				lo = hi
+				continue nextChunk
+			}
+			nextBase += blk.bytes
+		}
+
+		// If we get here, hi reached the end of the blocks without finding
+		// another zero-value block, so the rest of the blocks are an extent.
+		// In the typical case where nothing happened, return without packing.
+		if lo == 0 {
+			return []*extent{ext}
+		}
+		chunks = append(chunks, ext.blocks[lo:])
+		bases = append(bases, base)
+		sizes = append(sizes, nextBase-base)
+		break
+	}
+
+	exts := make([]*extent, len(chunks))
+	for i, chunk := range chunks {
+		exts[i] = &extent{
+			base:   bases[i],
+			bytes:  sizes[i],
+			blocks: chunk,
+		}
+	}
+	return exts
+}
+
+// A blockReader implements io.Reader for the concatenation of a slice of byte
+// slices. This avoids the overhead of constructing a bytes.Reader for each
+// blob plus an io.MultiReader to concatenate them.
+type blockReader struct {
+	cur    int
+	blocks [][]byte
+}
+
+func newBlockReader(blocks [][]byte) *blockReader {
+	return &blockReader{blocks: blocks}
+}
+
+func (r *blockReader) Read(data []byte) (int, error) {
+	var nr int
+	for nr < len(data) && r.cur < len(r.blocks) {
+		curBlock := r.blocks[r.cur]
+		cp := copy(data[nr:], curBlock)
+		if cp == len(curBlock) {
+			r.blocks[r.cur] = nil
+			r.cur++
+		}
+		nr += cp
+	}
+	if nr == 0 && r.cur >= len(r.blocks) {
+		return 0, io.EOF
+	}
+	return nr, nil
+}
+
+// zero sets all of data to zeroes and returns its length.
+func zero(data []byte) int {
+	for i := range data {
+		data[i] = 0
+	}
+	return len(data)
+}
+
+// isZero reports whether data is all zeroes.
+func isZero(data []byte) bool {
+	for _, b := range data {
+		if b != 0 {
+			return false
+		}
+	}
+	return true
+}
--- file/data.go
+++ file/data.go
@@ -158,72 +158,6 @@ func (d *fileData) truncate(ctx context.Context, s CAS, offset int64) error {
 	return nil
 }
 
-// splitExtent splits ext into possibly-multiple extents by removing
-// zero-valued data blocks. If there are no zero blocks, the return slice
-// contains just the original extent.
-func splitExtent(ext *extent) []*extent {
-	var chunks [][]cblock
-	var bases []int64
-	var sizes []int64
-
-	// Do a two-finger walk of the blocks. The left finger (lo) scans for the
-	// next non-zero block, and the right finger (hi) scans forward from there
-	// to find the end of the non-zero range. Along the way, we keep track of
-	// the base and size of each non-zero range, to pack into extents.
-
-	base := ext.base
-	lo := 0
-
-nextChunk:
-	for lo < len(ext.blocks) {
-		// Scan for a nonzero block.
-		if ext.blocks[lo].key == "" {
-			base += ext.blocks[lo].bytes
-			lo++
-			continue
-		}
-
-		// Scan forward for a zero block.
-		nextBase := base + ext.blocks[lo].bytes
-		for hi := lo + 1; hi < len(ext.blocks); hi++ {
-			blk := ext.blocks[hi]
-
-			// If we found a zero-value block, the non-zero blocks since the last
-			// marker are an extent.
-			if blk.key == "" {
-				chunks = append(chunks, ext.blocks[lo:hi])
-				bases = append(bases, base)
-				sizes = append(sizes, nextBase-base)
-				base = nextBase
-				lo = hi
-				continue nextChunk
-			}
-			nextBase += blk.bytes
-		}
-
-		// If we get here, hi reached the end of the blocks without finding
-		// another zero-value block, so the rest of the blocks are an extent.
-		// In the typical case where nothing happened, return without packing.
-		if lo == 0 {
-			return []*extent{ext}
-		}
-		chunks = append(chunks, ext.blocks[lo:])
-		bases = append(bases, base)
-		sizes = append(sizes, nextBase-base)
-		break
-	}
-
-	exts := make([]*extent, len(chunks))
-	for i, chunk := range chunks {
-		exts[i] = &extent{
-			base:   bases[i],
-			bytes:  sizes[i],
-			blocks: chunk,
-		}
-	}
-	return exts
-}
-
 // writeAt writes the contents of data at the specified offset in d.  It
 // returns the number of bytes successfully written, and satisfies the
 // semantics of io.WriterAt.
@@ -419,24 +353,6 @@ func (d *fileData) splitBlobs(ctx context.Context, s CAS, blobs ...[]byte) ([]cb
 	return blks, nil
 }
 
-// zero sets all of data to zeroes and returns its length.
-func zero(data []byte) int {
-	for i := range data {
-		data[i] = 0
-	}
-	return len(data)
-}
-
-// isZero reports whether data is all zeroes.
-func isZero(data []byte) bool {
-	for _, b := range data {
-		if b != 0 {
-			return false
-		}
-	}
-	return true
-}
-
 // splitSpan returns three subslices of the extents of d, those which end
 // entirely before offset lo, those fully containing the range from lo to hi,
 // and those which begin entirely at or after offset hi.
@@ -561,33 +477,4 @@ func (e *extent) findBlock(offset int64) (int, int64) {
 type cblock struct {
 	bytes int64  // number of bytes in the block
 	key   string // storage key for this block
-}
-
-// A blockReader implements io.Reader for the concatenation of a slice of byte
-// slices. This avoids the overhead of constructing a bytes.Reader for each
-// blob plus an io.MultiReader to concatenate them.
-type blockReader struct {
-	cur    int
-	blocks [][]byte
-}
-
-func newBlockReader(blocks [][]byte) *blockReader {
-	return &blockReader{blocks: blocks}
-}
-
-func (r *blockReader) Read(data []byte) (int, error) {
-	var nr int
-	for nr < len(data) && r.cur < len(r.blocks) {
-		curBlock := r.blocks[r.cur]
-		cp := copy(data[nr:], curBlock)
-		if cp == len(curBlock) {
-			r.blocks[r.cur] = nil
-			r.cur++
-		}
-		nr += cp
-	}
-	if nr == 0 && r.cur >= len(r.blocks) {
-		return 0, io.EOF
-	}
-	return nr, nil
 }
