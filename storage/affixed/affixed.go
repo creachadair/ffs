bdc19d4e43293fdbc6849a4b0826ab0ac6b27423
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-01-20 15:51:08 -0800

    storage: add affixed package
    This package combines the functionality of prefixed and suffixed.  They will be
    removed once the existing use is updated.

---
+++ storage/affixed/affixed.go
@@ -0,0 +1,205 @@
+// Copyright 2021 Michael J. Fromberger. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package affixed implements a blob.Store interface that delegates to another
+// Store, with keys namespaced by a fixed prefix and/or suffix concatenated
+// with each key.
+package affixed
+
+import (
+	"context"
+	"strings"
+
+	"github.com/creachadair/ffs/blob"
+)
+
+// Store implements the blob.Store interface by delegating to an underlying
+// store, but with each key prefixed and/or suffixed by fixed non-empty
+// strings.  This allows multiple consumers to share non-overlapping namespaces
+// within a single storage backend.
+type Store struct {
+	real   blob.Store
+	prefix string
+	suffix string
+}
+
+// New creates a Store associated with the specified s. The initial store is
+// exactly equivalent to the underlying store; use Derive to create clones that
+// use a different prefix.
+//
+// Affixes do not nest: If s is already an affixed.Store, it is returned as-is.
+func New(s blob.Store) Store {
+	if p, ok := s.(Store); ok {
+		return p
+	}
+	return Store{real: s}
+}
+
+// Base returns the underlying store associated with s.
+func (s Store) Base() blob.Store { return s.real }
+
+// WithPrefix creates a clone of s that delegates to the same underlying store,
+// but using a different prefix. The suffix, if any, is unchanged.
+func (s Store) WithPrefix(prefix string) Store {
+	return Store{real: s.real, prefix: prefix, suffix: s.suffix}
+}
+
+// WithSuffix creates a clone of s that delegates to the same underlying store,
+// but using a different suffix. The prefix, if any, is unchanged.
+func (s Store) WithSuffix(suffix string) Store {
+	return Store{real: s.real, prefix: s.prefix, suffix: suffix}
+}
+
+// Derive creates a clone of s that delegates to the same underlying store, but
+// using a different prefix and suffix. If prefix == suffix == "", Derive
+// returns a store that is equivalent to the original base store.
+func (s Store) Derive(prefix, suffix string) Store {
+	return Store{real: s.real, prefix: prefix, suffix: suffix}
+}
+
+// Prefix returns the key prefix associated with s.
+func (s Store) Prefix() string { return s.prefix }
+
+// Suffix returns the key suffix associated with s.
+func (s Store) Suffix() string { return s.suffix }
+
+func (s Store) wrapKey(key string) string { return s.prefix + key + s.suffix }
+
+func (s Store) unwrapKey(key string) string {
+	p := strings.TrimPrefix(key, s.prefix)
+	return strings.TrimSuffix(p, s.suffix)
+}
+
+// Close implements the optional blob.Closer interface. It delegates to the
+// underlying store if possible.
+func (s Store) Close(ctx context.Context) error { return s.real.Close(ctx) }
+
+// Get implements part of blob.Store by delegation.
+func (s Store) Get(ctx context.Context, key string) ([]byte, error) {
+	return s.real.Get(ctx, s.wrapKey(key))
+}
+
+// Put implements part of blob.Store by delegation.
+func (s Store) Put(ctx context.Context, opts blob.PutOptions) error {
+	// Leave the options as-given, except the key must be wrapped.
+	opts.Key = s.wrapKey(opts.Key)
+	return s.real.Put(ctx, opts)
+}
+
+// Delete implements part of blob.Store by delegation.
+func (s Store) Delete(ctx context.Context, key string) error {
+	return s.real.Delete(ctx, s.wrapKey(key))
+}
+
+// List implements part of blob.Store by delegation. It filters the underlying
+// list results to include only keys prefixed for this store.
+func (s Store) List(ctx context.Context, start string, f func(string) error) error {
+	// If we have no affixes, we do not need to filter.
+	if s.prefix == "" && s.suffix == "" {
+		return s.real.List(ctx, start, f)
+	}
+
+	// For the starting key, don't include the suffix, since the first match
+	// might not actually be there.
+	return s.real.List(ctx, s.prefix+start, func(wrappedKey string) error {
+		// Since keys are listed lexicographically, all the keys starting with
+		// our prefix should be grouped together. Thus, once we find any key that
+		// does NOT have our prefix, we can stop iterating.
+		if !strings.HasPrefix(wrappedKey, s.prefix) {
+			return blob.ErrStopListing
+		} else if !strings.HasSuffix(wrappedKey, s.suffix) {
+			return nil // not my key
+		}
+		return f(s.unwrapKey(wrappedKey))
+	})
+}
+
+// Len implements part of blob.Store by delegation. It reports only the number
+// of keys matching the current prefix.
+func (s Store) Len(ctx context.Context) (int64, error) {
+	// If the prefix and suffix are empty, we can delegate directly to the base.
+	if s.prefix == "" && s.suffix == "" {
+		return s.real.Len(ctx)
+	}
+
+	// Otherwise, we have to iterate.
+	var nk int64
+	err := s.real.List(ctx, s.prefix, func(cur string) error {
+		if !strings.HasPrefix(cur, s.prefix) {
+			return blob.ErrStopListing
+		} else if !strings.HasSuffix(cur, s.suffix) {
+			return nil // not my key
+		}
+		nk++
+		return nil
+	})
+	return nk, err
+}
+
+// CAS implements a prefixed wrapper around a blob.CAS instance.
+type CAS struct {
+	Store
+	cas blob.CAS
+}
+
+// NewCAS creates a new prefixed Store associated with the specified cas.
+// Prefixes do not nest: If cas is already a prefixed.CAS, it is returned as.is.
+func NewCAS(cas blob.CAS) CAS {
+	if p, ok := cas.(CAS); ok {
+		return p
+	}
+	return CAS{Store: New(cas), cas: cas}
+}
+
+// Base returns the underlying store associated with c.
+func (c CAS) Base() blob.CAS { return c.cas }
+
+// WithPrefix creates a clone of c that delegates to the same underlying store,
+// but using a different prefix. The suffix, if any, is unchanged.
+func (c CAS) WithPrefix(prefix string) CAS {
+	return CAS{Store: c.Store.WithPrefix(prefix), cas: c.cas}
+}
+
+// WithSuffix creates a clone of c that delegates to the same underlying store,
+// but using a different suffix. The prefix, if any, is unchanged.
+func (c CAS) WithSuffix(suffix string) CAS {
+	return CAS{Store: c.Store.WithSuffix(suffix), cas: c.cas}
+}
+
+// Derive creates a clone of c that delegates to the same underlying store, but
+// using a different prefix and suffix. If prefix == suffix == "", Derive
+// returns a store that is equivalent to the original base store.
+func (c CAS) Derive(prefix, suffix string) CAS {
+	return CAS{Store: c.Store.Derive(prefix, suffix), cas: c.cas}
+}
+
+func (c CAS) setOptions(opts blob.CASPutOptions) blob.CASPutOptions {
+	return blob.CASPutOptions{
+		Data:   opts.Data,
+		Prefix: opts.Prefix + c.Store.prefix,
+		Suffix: opts.Suffix + c.Store.suffix,
+	}
+}
+
+// CASPut implements part of the blob.CAS interface.
+func (c CAS) CASPut(ctx context.Context, opts blob.CASPutOptions) (string, error) {
+	key, err := c.cas.CASPut(ctx, c.setOptions(opts))
+	return strings.TrimPrefix(key, c.Store.prefix), err
+}
+
+// CASKey implements part of the blob.CAS interface.
+func (c CAS) CASKey(ctx context.Context, opts blob.CASPutOptions) (string, error) {
+	key, err := c.cas.CASKey(ctx, c.setOptions(opts))
+	return c.unwrapKey(key), err
+}
---
+++ storage/affixed/affixed_test.go
@@ -0,0 +1,206 @@
+// Copyright 2021 Michael J. Fromberger. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package affixed_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/creachadair/ffs/blob"
+	"github.com/creachadair/ffs/blob/memstore"
+	"github.com/creachadair/ffs/blob/storetest"
+	"github.com/creachadair/ffs/storage/affixed"
+	"github.com/google/go-cmp/cmp"
+)
+
+var (
+	_ blob.Store = affixed.Store{}
+	_ blob.CAS   = affixed.CAS{}
+)
+
+func TestStore(t *testing.T) {
+	m := memstore.New()
+	p := affixed.New(m).Derive("POG:", ":CHAMP")
+	storetest.Run(t, p)
+}
+
+func mustPut(t *testing.T, s blob.Store, key, val string) {
+	t.Helper()
+	if err := s.Put(context.Background(), blob.PutOptions{
+		Key:     key,
+		Data:    []byte(val),
+		Replace: true,
+	}); err != nil {
+		t.Errorf("Put %q=%q failed: %v", key, val, err)
+	}
+}
+
+func runList(p blob.Store, want ...string) func(t *testing.T) {
+	return func(t *testing.T) {
+		var got []string
+		if err := p.List(context.Background(), "", func(key string) error {
+			got = append(got, key)
+			return nil
+		}); err != nil {
+			t.Fatalf("List p1: %v", err)
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("p keys: (-want, +got)\n%s", diff)
+		}
+	}
+}
+
+func TestAffixes(t *testing.T) {
+	m := memstore.New()
+	p1 := affixed.New(m).Derive("A:", ":A")
+	p2 := p1.Derive("B:", ":B")
+	p3 := affixed.NewCAS(selfCAS{m}).Derive("C:", ":C")
+
+	// Verify that the keys that arrive in the underlying store reflect the
+	// correct affixes, and that the namespaces are disjoint as long as the
+	// affixes are disjoint.
+	mustPut(t, p1, "foo", "bar")
+	mustPut(t, p2, "foo", "baz")
+	mustPut(t, p1, "xyzzy", "plugh")
+	mustPut(t, p2, "foo", "quux")
+	mustPut(t, p2, "bar", "plover")
+	mustPut(t, p3, "foo", "bizzle")
+	mustPut(t, p3, "zuul", "dana")
+
+	// Verify that a CAS key is properly affixed.
+	ckey, err := p3.CASPut(context.Background(), blob.CASPutOptions{Data: []byte("hexxus")})
+	if err != nil {
+		t.Errorf("p3 CAS put: %v", err)
+	}
+
+	t.Run("Snapshot", func(t *testing.T) {
+		snap := m.Snapshot(make(map[string]string))
+
+		if diff := cmp.Diff(map[string]string{
+			"A:foo:A":   "bar",
+			"A:xyzzy:A": "plugh",
+			"B:foo:B":   "quux",
+			"B:bar:B":   "plover",
+			"C:foo:C":   "bizzle",
+			"C:zuul:C":  "dana",   // from p3.Put
+			"C:" + ckey: "hexxus", // from p3.CASPut
+		}, snap); diff != "" {
+			t.Errorf("Affixed store: wrong content (-want, +got)\n%s", diff)
+		}
+	})
+
+	t.Run("List-1", runList(p1, "foo", "xyzzy"))
+	t.Run("List-2", runList(p2, "bar", "foo"))
+	t.Run("List-3", runList(p3, "foo", "hexxus", "zuul"))
+}
+
+func TestLen(t *testing.T) {
+	m := memstore.New()
+	p0 := affixed.New(m)
+	p1 := p0.Derive("X:", ":X")
+	p2 := p1.WithPrefix("Y:")
+
+	mustPut(t, p0, "starfruit", "0")
+	mustPut(t, p1, "apple", "1")
+	mustPut(t, p1, "pear", "1")
+	mustPut(t, p2, "plum", "2")
+	mustPut(t, p2, "cherry", "2")
+	mustPut(t, p2, "mango", "2")
+
+	tests := []struct {
+		store blob.Store
+		want  int64
+	}{
+		{m, 6},  // base store: all the keys
+		{p0, 6}, // empty prefix, equivalent to base
+		{p1, 2},
+		{p2, 3},
+	}
+	for _, test := range tests {
+		got, err := test.store.Len(context.Background())
+		if err != nil {
+			t.Errorf("Len failed: %v", err)
+			continue
+		}
+		if got != test.want {
+			t.Errorf("Len %+v: got %v, want %v", test.store, got, test.want)
+		}
+	}
+}
+
+func TestNesting(t *testing.T) {
+	m := memstore.New()
+	p1 := affixed.New(m)
+	c1 := affixed.NewCAS(selfCAS{m})
+
+	t.Run("Renew", func(t *testing.T) {
+		if p2 := affixed.New(p1); p2 != p1 {
+			t.Errorf("Wrapped new: got %v, want %v", p2, p1)
+		}
+	})
+	t.Run("RenewCAS", func(t *testing.T) {
+		if c2 := affixed.NewCAS(c1); c2 != c1 {
+			t.Errorf("Wrapped new: got %v, want %v", c2, c1)
+		}
+	})
+
+	p2 := p1.Derive("X:", ":X")
+	p3 := p1.Derive("Y:", ":Y")
+	p4 := p2.Derive("Z:", ":Z") // derivation replaces existing keys
+	p5 := p2.Derive("", "")     // empty affix goes back to the original
+	p6 := p2.WithPrefix("Q:")   // as p2, with prefix altered
+	p7 := p2.WithSuffix(":Q")   // as P2, with suffix altered
+
+	mustPut(t, p1, "foo", "1")
+	mustPut(t, p2, "foo", "2")
+	mustPut(t, p3, "foo", "3")
+	mustPut(t, p4, "foo", "4")
+	mustPut(t, p5, "bar", "5")
+	mustPut(t, p6, "bar", "6")
+	mustPut(t, p7, "bar", "7")
+
+	t.Run("Snapshot", func(t *testing.T) {
+		snap := m.Snapshot(make(map[string]string))
+
+		if diff := cmp.Diff(map[string]string{
+			"foo":     "1", // unaffixed from p1
+			"X:foo:X": "2", // from p2
+			"Y:foo:Y": "3", // from p3
+			"Z:foo:Z": "4", // from p4
+			"bar":     "5", // from p5 (eqv. to p1)
+			"Q:bar:X": "6", // from p6
+			"X:bar:Q": "7", // from p7
+		}, snap); diff != "" {
+			t.Errorf("Affixed store: wrong content (-want, +got)\n%s", diff)
+		}
+	})
+}
+
+type selfCAS struct {
+	blob.Store
+}
+
+func (selfCAS) CASKey(_ context.Context, opts blob.CASPutOptions) (string, error) {
+	return opts.Prefix + string(opts.Data) + opts.Suffix, nil
+}
+
+func (s selfCAS) CASPut(ctx context.Context, opts blob.CASPutOptions) (string, error) {
+	key := opts.Prefix + string(opts.Data) + opts.Suffix
+	err := s.Put(ctx, blob.PutOptions{Key: key, Data: opts.Data})
+	if err != nil && !blob.IsKeyExists(err) {
+		return key, err
+	}
+	return key, nil
+}
