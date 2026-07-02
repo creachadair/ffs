4b4bf6f9857d15160ca66228ed0b845b582f6d29
M. J. Fromberger <michael.j.fromberger@gmail.com>
2026-07-02 11:50:25 -0700

    filetree: add SetPath method

--- filetree/filetree.go
+++ filetree/filetree.go
@@ -20,7 +20,9 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"fmt"
+	"io/fs"
 	"path"
 	"strings"
 
@@ -135,6 +137,57 @@ func (s Store) OpenPath(ctx context.Context, path string) (*PathInfo, error) {
 	out.File = tf
 	out.FileKey = out.File.Key() // safe, it was just opened
 	return out, nil
+}
+
+// SetPath sets the specified root-key/path or file-key/path to the given
+// target file. It returns the storage key of the resulting updated object.
+//
+// If path has only a root-key, the base file of that root is replaced.
+// If path has only a file-key (and no subpath), it is an error.
+func (s Store) SetPath(ctx context.Context, path string, tf *file.File) (string, error) {
+	obase, orest := SplitPath(path)
+	if orest == "." {
+		orest = "" // setting root
+	}
+
+	of, err := s.OpenPath(ctx, obase) // N.B. No path; see below
+	if err != nil {
+		return "", err
+	}
+
+	// If orest == "", we are being asked to the target file of a root.
+	if orest == "" {
+		// If we don't have a root, we can't do anything.
+		if of.Root == nil {
+			return "", errors.New("cannot set the root of a file-key")
+		}
+
+		// Otherwise, flush the file and update the root.
+		key, err := tf.Flush(ctx)
+		if err != nil {
+			return "", err
+		}
+		if key != of.Root.FileKey {
+			of.Root.IndexKey = "" // invalidate the index, the key changed
+		}
+		of.Root.FileKey = key
+		return key, of.Root.Save(ctx, of.RootKey)
+	}
+
+	// Otherwise, we're hooking something below another object.
+	if _, err := fpath.Set(ctx, of.Base, orest, &fpath.SetOptions{
+		Create: true,
+		SetStat: func(st *file.Stat) {
+			if st.Mode == 0 {
+				st.Mode = fs.ModeDir | 0755
+			}
+		},
+		File: tf,
+	}); err != nil {
+		return "", err
+	}
+
+	return of.Flush(ctx)
 }
 
 // LoadIndex opens an [index.Index] from the specified key in s.
