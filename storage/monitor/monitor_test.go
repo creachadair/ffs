669e74df3a9d9ff24a8dad7592f4eae82eb1993d
M. J. Fromberger <michael.j.fromberger@gmail.com>
2026-07-20 21:32:23 -0700

    storage/monitor: use storetest to exercise the plumbing

--- storage/monitor/monitor_test.go
+++ storage/monitor/monitor_test.go
@@ -15,10 +15,50 @@
 package monitor_test
 
 import (
+	"context"
+	"testing"
+
 	"github.com/creachadair/ffs/blob"
+	"github.com/creachadair/ffs/blob/storetest"
+	"github.com/creachadair/ffs/storage/dbkey"
+	"github.com/creachadair/ffs/storage/filestore"
 	"github.com/creachadair/ffs/storage/monitor"
 )
 
 type kvStub struct{ blob.KV }
 
 var _ blob.Store = (*monitor.M[any, kvStub])(nil)
+
+type testDB = filestore.Store
+type testKV = filestore.KV
+
+type testStore struct {
+	*monitor.M[testDB, testKV]
+}
+
+func (t testStore) Close(ctx context.Context) error { return t.M.DB.Close(ctx) }
+
+func TestMonitor(t *testing.T) {
+	fs, err := filestore.New(t.TempDir())
+	if err != nil {
+		t.Fatalf("Create base store: %v", err)
+	}
+	s := testStore{M: monitor.New(monitor.Config[testDB, testKV]{
+		DB: fs,
+		NewKV: func(ctx context.Context, fs testDB, _ dbkey.Prefix, name string) (testKV, error) {
+			kv, err := fs.KV(ctx, name)
+			if err != nil {
+				return testKV{}, err
+			}
+			return kv.(testKV), nil
+		},
+		NewSub: func(ctx context.Context, fs testDB, _ dbkey.Prefix, name string) (testDB, error) {
+			sub, err := fs.Sub(ctx, name)
+			if err != nil {
+				return testDB{}, err
+			}
+			return sub.(testDB), nil
+		},
+	})}
+	storetest.Run(t, s)
+}
